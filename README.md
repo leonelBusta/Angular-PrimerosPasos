b55bb0f6782ecd42c70d160e5c3b7b8e4690ff9a
leonel <leonelbustamant.ues@gmail.com>
2024-02-28 16:05:34 -0600

    formularios

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Buenas Practicas de Angular 
+# Buenas Practicas de Angular 🛡
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
 
--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contactos',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, // Forms de tipo plantilla
+    ReactiveFormsModule, // formario de tipo reactivo
+    CommonModule,
+  ],
   templateUrl: './contactos.component.html',
-  styleUrl: './contactos.component.css'
+  styleUrl: './contactos.component.css',
 })
-export class ContactosComponent {
-
+export class ContactosComponent implements OnInit {
+  formularioContacto: FormGroup;
+  tipoDni: string= 'DNI';
+  /*usuarioActivo: any = {
+    nombre: 'Leonel',
+    apellido: 'Bustamante',
+    dni: '12345678',
+  }
+*/
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      tipoDni: ['', ],
+      dni: ['', ],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+  ngOnInit(): void {
+    this.formularioContacto.get('tipoDni')?.valueChanges.subscribe(value =>{
+      this.tipoDni = value;
+    });
+      /* this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo)
+      this.formularioContacto.patchValue({
+        nombre: this.usuarioActivo.nombre,
+        apellido: this.usuarioActivo.apellido,
+        dni: this.usuarioActivo.dni,
+      })
+      this.formularioContacto.get('nombre')?.disable()
+      this.formularioContacto.get('apellido')?.disable()
+      this.formularioContacto.get('dni')?.disable()
+*/
+  }
+  hasErrors(controlName: string, errorType: string) {
+    return (
+      this.formularioContacto.get(controlName)?.hasError(errorType) &&
+      this.formularioContacto.get(controlName)?.touched
+    );
+  }
+  enviar() {
+    console.log(this.formularioContacto);
+  }
 }
